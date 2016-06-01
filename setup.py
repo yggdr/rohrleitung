355b1ad52c6cd344cf18c09e7c2475f3f95bc709
Konstantin Schukraft <konstantin@schukraft.org>
2016-06-01 16:32:39 +0200

    Fix missing MANIFEST, now installable via pip

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='rohrleitung',
-    version='0.1',
+    version='0.1.1',
     description='pipelining generators like python, not like shell',
     long_description=long_description,
     url='https://github.com/yggdr/rohrleitung',
